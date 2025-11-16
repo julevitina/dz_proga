d1fbf535ff1b9f1764d8dc4e8b8f1d12975f1c42
julevitina <julevitina@knu.ua>
2025-11-16 13:49:10 +0200

    Initial commit

--- dz_9.cpp
+++ dz_9.cpp
@@ -1,9 +1,15 @@
 #include<iostream>
+#include<format>
 using namespace std;
+int i=6, n=8, m=4;
 int main() {
-    int n=3, m=5;
-    cout << --n + ++m << endl;
-    cout << n++ + m-- << endl;
-    cout << n << " " << m << endl;
+   do {
+    cout << i << ' ' << n << ' ' << m << "|\n";
+    i += 1;
+    n*= -i;
+    m *= 2;
+   }
+    while (n<m);
+    cout << i << ' ' << n << ' ' << m << "|\n";
     return 0;
 }
